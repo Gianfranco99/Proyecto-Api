d1bae95136f7d3a23d96f1882a0258591cd8fee4
Gianfranco99 <gianfranco.benvenuto99@gmail.com>
2021-02-03 16:27:07 -0300

    cambio de foto

---
+++ build/precache-manifest.3dca559ec03bfc5d6b7dd77cb4a4c19c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc665868663048372e2dcc240147855",
+    "url": "/Weather-Api/index.html"
+  },
+  {
+    "revision": "7592013f60a56c0874ee",
+    "url": "/Weather-Api/static/css/main.c04997d2.chunk.css"
+  },
+  {
+    "revision": "336d61c078bfb1077665",
+    "url": "/Weather-Api/static/js/2.8c7b534a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Weather-Api/static/js/2.8c7b534a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7592013f60a56c0874ee",
+    "url": "/Weather-Api/static/js/main.dcfad40a.chunk.js"
+  },
+  {
+    "revision": "4dd4350684141ca67ea8",
+    "url": "/Weather-Api/static/js/runtime-main.9c658820.js"
+  },
+  {
+    "revision": "439b50eb4b3438359223d694c659073d",
+    "url": "/Weather-Api/static/media/asutralia.439b50eb.jpg"
+  },
+  {
+    "revision": "dce43b636bc14e00ede6f127fa2fb42e",
+    "url": "/Weather-Api/static/media/ciudad 2.dce43b63.jpg"
+  },
+  {
+    "revision": "fbec24a96b84688b1397e0b2be100d65",
+    "url": "/Weather-Api/static/media/ciudad1.fbec24a9.jpg"
+  },
+  {
+    "revision": "e48264fb2c52ffa08d5cdb6b7a8ddcbf",
+    "url": "/Weather-Api/static/media/nueva zelanda.e48264fb.jpg"
+  }
+]);
